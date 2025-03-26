933d6a7b9817221aeaac1086f3d382e832ccf4c1
L0772L <nsdb0772@leeds.ac.uk>
2025-03-26 19:55:16 +0000

    added work

--- Xor.asm
+++ Xor.asm
@@ -1,21 +1,12 @@
-// Xor.asm
-@R0
-D=M      // D = x
-@R1
-D=D&M    // D = x AND y
-@temp
-M=D      // temp = x AND y
-
-@temp
-D=!M     // D = NOT (x AND y)
-
-@R0
-D=D|M    // D = (NOT (x AND y)) OR x
-@R1
-D=D|M    // D = (NOT (x AND y)) OR x OR y
-
-@R2
-M=D      // R2 = x ⊕ y
-(END)
-@END
-0;JMP    // 无限循环
+@0
+D=!M
+@1
+D=D&M
+@2
+M=D
+@1
+D=!M
+@0
+D=D&M
+@2
+M=D|M
